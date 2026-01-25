d51f712efc3db51ce645365f78c6b01e2df072ae
Kauboimausu <malagonngasca@gmail.com>
2026-01-25 01:01:48 -0600

    Finished css for left part of the page

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -8,41 +8,40 @@ export default function GeneralInfo({ personalInfo, onSubmit }) {
 	}
 
 	return (
-		<form className="general info-form" onSubmit={(e) => handleSubmit(e)}>
-			<div className="input-wrap">
-				<label htmlFor="name">Name:</label>
-				<input
-					type="text"
-					name="name"
-					id="name"
-					defaultValue={personalInfo.name}
-					required
-				/>
-			</div>
-
-			<div className="input-wrap">
-				<label htmlFor="email">Email:</label>
-				<input
-					type="email"
-					name="email"
-					id="email"
-					defaultValue={personalInfo.email}
-					required
-				/>
-			</div>
-
-			<div className="input-wrap">
-				<label htmlFor="phone-number">Phone Number:</label>
-				<input
-					type="tel"
-					name="phone-number"
-					id="phone-number"
-					defaultValue={personalInfo.phone}
-					required
-				/>
-			</div>
-
-			<button type="submit">Submit</button>
-		</form>
+		<div className="form-wrap">
+			<form className="general info-form" onSubmit={(e) => handleSubmit(e)}>
+				<div className="input-wrap">
+					<label htmlFor="name">Name:</label>
+					<input
+						type="text"
+						name="name"
+						id="name"
+						defaultValue={personalInfo.name}
+						required
+					/>
+				</div>
+				<div className="input-wrap">
+					<label htmlFor="email">Email:</label>
+					<input
+						type="email"
+						name="email"
+						id="email"
+						defaultValue={personalInfo.email}
+						required
+					/>
+				</div>
+				<div className="input-wrap">
+					<label htmlFor="phone-number">Phone Number:</label>
+					<input
+						type="tel"
+						name="phone-number"
+						id="phone-number"
+						defaultValue={personalInfo.phone}
+						required
+					/>
+				</div>
+				<button type="submit" className="submit">Submit</button>
+			</form>
+		</div>
 	)
 }
